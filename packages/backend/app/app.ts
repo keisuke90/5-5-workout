4b0b11cd20a46ea6982adace65ddcad97b703171
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-02-05 16:54:49 +0900

    cors対応（暫定）

--- packages/backend/app/app.ts
+++ packages/backend/app/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import { logger } from "./lib/log/logger";
 import { workoutRouter } from "./routes/workout";
 import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/workout", workoutRouter);
 
