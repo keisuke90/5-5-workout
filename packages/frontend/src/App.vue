4d581e4cefcbc52dd2add9bdd8f27f9c2e22a440
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-02-07 09:17:35 +0900

    リクエスト時の数値を入力できるようにした

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -2,15 +2,26 @@
 import { ref, onMounted } from "vue";
 import WorkoutApiService from "./services/workoutApi";
 
-let workout: any = ref({});
-onMounted(() => {
-  WorkoutApiService.fiveByFiveGenerate(100).then((res: any) => {
-    workout.value = res.data;
-  });
-});
+let weight = ref();
+let workout = ref();
+const fiveByFive = (weight: number) => {
+  WorkoutApiService.fiveByFiveGenerate(weight)
+    .then((res: any) => {
+      workout.value = res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 </script>
 
 <template>
+  <input
+    type="number"
+    v-model="weight"
+    placeholder="kg"
+    @input="fiveByFive(weight)"
+  />
   <div class="table">
     {{ workout }}
   </div>
