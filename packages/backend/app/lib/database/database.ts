c0e3ce6154ac59465713c0690b396c44e49a5d73
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-02-15 09:25:03 +0900

    tweak

--- packages/backend/app/lib/database/database.ts
+++ packages/backend/app/lib/database/database.ts
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 
 export const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_PORT || "admin",
+  user: process.env.MYSQL_USERNAME || "admin",
   password: process.env.MYSQL_PASSWORD || "admin",
   database: process.env.DATABASE || "workout_generator",
 });
