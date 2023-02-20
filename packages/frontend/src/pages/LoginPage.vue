cf59f0d3105ebc3b343e391692b1d43f6e70872b
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-02-20 23:24:47 +0900

    ログインページ実装

--- packages/frontend/src/pages/LoginPage.vue
+++ packages/frontend/src/pages/LoginPage.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>
-    <h1>this is login</h1>
-  </div>
+  <form>
+    <h1>ログイン</h1>
+    <label for="username">ユーザー名</label>
+    <input type="text" id="username" name="username" required />
+    <label for="password">パスワード</label>
+    <input type="password" id="password" name="password" required />
+    <input type="submit" value="ログイン" />
+  </form>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+form {
+  background-color: #fff;
+  padding: 20px;
+  width: 400px;
+  margin: 50px auto;
+  border-radius: 10px;
+  box-shadow: 0 0 10px #ccc;
+}
+input[type="text"],
+input[type="password"] {
+  display: block;
+  margin: 10px 0;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 100%;
+  font-size: 16px;
+  box-sizing: border-box;
+}
+input[type="submit"] {
+  background-color: #4caf50;
+  color: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  font-size: 16px;
+}
+</style>
