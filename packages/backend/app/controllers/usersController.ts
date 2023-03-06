14832efedfde10ea77d657574c9a75b25763e8ce
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-03-06 22:52:49 +0900

    不要な記述を削除

--- packages/backend/app/controllers/usersController.ts
+++ packages/backend/app/controllers/usersController.ts
@@ -43,7 +43,6 @@ export const insertUser = async (
   next: NextFunction
 ) => {
   try {
-    // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
